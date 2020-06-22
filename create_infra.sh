fb8ce7c809e3c8c54f3b9ccb588c3b1a59290abc
root <root@ip-172-31-55-195.ec2.internal>
2020-06-22 18:06:54 +0000

    create infra completed

--- create_infra.sh
+++ create_infra.sh
@@ -8,3 +8,9 @@ mkdir -p ~/.aws
 cp -v config ~/.aws
 cd ecr-repo
 ecr_url=`terraform apply -auto-approve -parallelism=50 | grep "opstree =" | cut -d'=' -f2`
+ecr_repo=`echo ${ecr_url} | cut -d/ -f1`
+cd -
+docker build -t opstree/spring3hibernate:latest -f Dockerfile .
+docker tag opstree/spring3hibernate ${ecr_repo}/opstree:latest
+$(aws ecr get-login --no-include-email)
+docker push ${ecr_repo}/opstree:latest
