ef495a08415ade90e3dc8aad251c9f880d6ff888
root <root@ip-172-31-59-249.ec2.internal>
2020-06-23 18:49:16 +0000

    added separate vpc

---
+++ vpc/variables.tf
@@ -0,0 +1,91 @@
+variable "aws_region" {
+  description = "The AWS region to create things in."
+  default     = "us-east-1"
+}
+
+variable "az_count" {
+  description = "Number of AZs to cover in a given AWS region"
+  default     = "2"
+}
+
+variable "key_name" {
+  description = "Name of AWS key pair"
+}
+
+variable "instance_type" {
+  default     = "t2.small"
+  description = "AWS instance type"
+}
+
+variable "asg_min" {
+  description = "Min numbers of servers in ASG"
+  default     = "1"
+}
+
+variable "asg_max" {
+  description = "Max numbers of servers in ASG"
+  default     = "2"
+}
+
+variable "asg_desired" {
+  description = "Desired numbers of servers in ASG"
+  default     = "1"
+}
+
+variable "service_desired" {
+  description = "Desired numbers of instances in the ecs service"
+  default     = "1"
+}
+
+variable "admin_cidr_ingress" {
+  description = "CIDR to allow tcp/22 ingress to EC2 instance"
+}
+
+# RDS
+variable "identifier" {
+  default     = "mydb-rds"
+  description = "Identifier for your DB"
+}
+
+variable "storage" {
+  default     = "10"
+  description = "Storage size in GB"
+}
+
+variable "engine" {
+  default     = "mysql"
+  description = "Engine type, example values mysql, postgres"
+}
+
+variable "engine_version" {
+  description = "Engine version"
+
+  default = {
+    mysql    = "5.7.21"
+    postgres = "9.6.8"
+  }
+}
+
+variable "instance_class" {
+  default     = "db.t2.micro"
+  description = "Instance class"
+}
+
+variable "db_name" {
+  default     = "mydb"
+  description = "db name"
+}
+
+variable "username" {
+  default     = "root"
+  description = "User name"
+}
+
+variable "password" {
+  description = "password, provide through your ENV variables"
+}
+
+variable "image_url" {
+  default     = "881730508732.dkr.ecr.us-east-1.amazonaws.com/opstree"
+  description = "docker image"
+}
