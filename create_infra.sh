20bd1b90df4dfe85ac35a88fa3a81f53d3eef015
root <root@ip-172-31-59-115.ec2.internal>
2020-06-27 10:06:51 +0000

    added rds detail in data properties

--- create_infra.sh
+++ create_infra.sh
@@ -18,4 +18,12 @@ docker push ${ecr_repo}/opstree:latest
 cd ecs-alb-rds
 sed -i "s|opstree|${ecr_repo}/opstree:latest|g" variables.tf
 terraform init
+rds_endpoint=`terraform apply -auto-approve -parallelism=50 | grep db_instance_address | cut -d'=' -f2 | sed -e 's/^[ \t]*//'`
+cd -
+sed -i "s|mysql.okts.tk|${rds_endpoint}|g" src/main/resources/database.properties
+docker build -t opstree/spring3hibernate:latest -f Dockerfile .
+docker tag opstree/spring3hibernate ${ecr_repo}/opstree:latest
+$(aws ecr get-login --no-include-email)
+docker push ${ecr_repo}/opstree:latest
+cd ecs-alb-rds
 terraform apply -auto-approve -parallelism=50
